e0d7bcba04c209611bc6440e976fc72958ea932e
etacodes <75968126+etacodes@users.noreply.github.com>
2022-10-17 22:34:53 +0700

    menyelesaikan blade templating engine

--- routes/web.php
+++ routes/web.php
@@ -14,17 +14,93 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view ('home');
+    return view ('home', [
+        "title" => "Home"
+    ]);
 });
 
 Route::get('/about', function () {
     return view ('about', [
+        "title" => "About",
         "name" => "Ericko Timur Apandi",
         "email" => "erickotimur2002@gmail.com",
         "image" => "oke.jpeg"
     ]);
 });
 
+
+
+
 Route::get('/blog', function () {
-    return view ('posts');
+    $blog_posts = [
+        [
+        "title" => "Judul Post Pertama",
+        "slug" => "judul-post-pertama",
+        "author" => "Ericko Timur Apandi",
+        "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis rerum incidunt porro nulla nostrum optio delectus, 
+        temporibus repellat magni unde esse aspernatur quaerat reiciendis at tenetur quos similique modi et sequi totam suscipit! Ut magnam 
+        mollitia error, odio ratione deserunt dignissimos nulla aspernatur aliquam aperiam quas! Quibusdam cumque recusandae explicabo sed vitae 
+        animi, sequi quo soluta eum quidem ut aut eligendi eveniet perferendis laborum harum quod nobis
+        inventore. Alias explicabo accusamus praesentium quos? Quaerat quidem dolor nesciunt officia facere quisquam."
+        ],
+    
+        [
+        "title" => "Judul Post Kedua",
+        "slug" => "judul-post-kedua",
+        "author" => "Renandra Rahadian Putri",
+        "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis rerum incidunt porro nulla nostrum optio delectus, 
+        temporibus repellat magni unde esse aspernatur quaerat reiciendis at tenetur quos similique modi et sequi totam suscipit! Ut magnam 
+        mollitia error, odio ratione deserunt dignissimos nulla aspernatur aliquam aperiam quas! Quibusdam cumque recusandae explicabo sed vitae 
+        animi, sequi quo soluta eum quidem ut aut eligendi eveniet perferendis laborum harum quod nobis
+        inventore. Alias explicabo accusamus praesentium quos? Quaerat quidem dolor nesciunt officia facere quisquam."
+        ], 
+            
+        ];
+
+    return view ('posts', [
+        "title" => "Posts",
+        "posts" => $blog_posts
+    ]);
+});
+
+
+//Halaman single route
+
+Route::get('posts/{slug}', function($slug) {
+    $blog_posts = [
+        [
+        "title" => "Judul Post Pertama",
+        "slug" => "judul-post-pertama",
+        "author" => "Ericko Timur Apandi",
+        "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis rerum incidunt porro nulla nostrum optio delectus, 
+        temporibus repellat magni unde esse aspernatur quaerat reiciendis at tenetur quos similique modi et sequi totam suscipit! Ut magnam 
+        mollitia error, odio ratione deserunt dignissimos nulla aspernatur aliquam aperiam quas! Quibusdam cumque recusandae explicabo sed vitae 
+        animi, sequi quo soluta eum quidem ut aut eligendi eveniet perferendis laborum harum quod nobis
+        inventore. Alias explicabo accusamus praesentium quos? Quaerat quidem dolor nesciunt officia facere quisquam."
+        ],
+    
+        [
+        "title" => "Judul Post Kedua",
+        "slug" => "judul-post-kedua",
+        "author" => "Renandra Rahadian Putri",
+        "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis rerum incidunt porro nulla nostrum optio delectus, 
+        temporibus repellat magni unde esse aspernatur quaerat reiciendis at tenetur quos similique modi et sequi totam suscipit! Ut magnam 
+        mollitia error, odio ratione deserunt dignissimos nulla aspernatur aliquam aperiam quas! Quibusdam cumque recusandae explicabo sed vitae 
+        animi, sequi quo soluta eum quidem ut aut eligendi eveniet perferendis laborum harum quod nobis
+        inventore. Alias explicabo accusamus praesentium quos? Quaerat quidem dolor nesciunt officia facere quisquam."
+        ], 
+            
+        ];
+
+        $new_post = [];
+        foreach($blog_posts as $post) {
+            if($post["slug"] === $slug) {
+                $new_post = $post;
+        }
+    }
+
+    return view('post', [
+        "title" => "Single Post",
+        "post" => $new_post
+    ]);
 });
