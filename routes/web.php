cbc5f66949261d6b75046980732fbe6499896c06
etacodes <75968126+etacodes@users.noreply.github.com>
2022-10-17 19:26:16 +0700

    menyelesaikan struktur folder,routes & view

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view ('home');
+});
+
+Route::get('/about', function () {
+    return view ('about', [
+        "name" => "Ericko Timur Apandi",
+        "email" => "erickotimur2002@gmail.com",
+        "image" => "oke.jpeg"
+    ]);
+});
+
+Route::get('/blog', function () {
+    return view ('posts');
 });
