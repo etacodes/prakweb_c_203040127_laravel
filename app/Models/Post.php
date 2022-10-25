d4902f072fb93bb24c0b5161504e938a7afa7df0
etacodes <75968126+etacodes@users.noreply.github.com>
2022-10-26 02:56:44 +0700

    Post Model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,46 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-
-class Post 
+class Post extends Model
 {
-    private static $blog_posts = [
-        [
-        "title" => "Judul Tulisan Pertama",
-        "slug" => "judul-tulisan-pertama",
-        "author" => "Ericko Timur Apandi",
-        "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis rerum incidunt porro nulla nostrum optio delectus, 
-        temporibus repellat magni unde esse aspernatur quaerat reiciendis at tenetur quos similique modi et sequi totam suscipit! Ut magnam 
-        mollitia error, odio ratione deserunt dignissimos nulla aspernatur aliquam aperiam quas! Quibusdam cumque recusandae explicabo sed vitae 
-        animi, sequi quo soluta eum quidem ut aut eligendi eveniet perferendis laborum harum quod nobis
-        inventore. Alias explicabo accusamus praesentium quos? Quaerat quidem dolor nesciunt officia facere quisquam."
-        ],
-    
-        [
-        "title" => "Judul Tulisan Kedua",
-        "slug" => "judul-tulisan-kedua",
-        "author" => "Renandra Rahadian Putri",
-        "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis rerum incidunt porro nulla nostrum optio delectus, 
-        temporibus repellat magni unde esse aspernatur quaerat reiciendis at tenetur quos similique modi et sequi totam suscipit! Ut magnam 
-        mollitia error, odio ratione deserunt dignissimos nulla aspernatur aliquam aperiam quas! Quibusdam cumque recusandae explicabo sed vitae 
-        animi, sequi quo soluta eum quidem ut aut eligendi eveniet perferendis laborum harum quod nobis
-        inventore. Alias explicabo accusamus praesentium quos? Quaerat quidem dolor nesciunt officia facere quisquam."
-        ], 
-            
-        ];
+    use HasFactory;
 
-        public static function all ()
-        {
-            return collect(self::$blog_posts);
-        }
-
-        public static function find ($slug)
-        {
-            $posts = static::all();
-   
-
-    return $posts->firstWhere('slug', $slug);
-}
-
+    //protected $fillable = ['title', 'excerpt', 'body'];
+    protected $guarded = ['id'];
 }
