c69d9dc2d8d0cce357cef5a8e6c03959f58d92af
etacodes <75968126+etacodes@users.noreply.github.com>
2022-10-27 00:08:38 +0700

    factory & faker

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 
 use App\Models\Category;
+use App\Models\User;
 
 /*
 |--------------------------------------------------------------------------
@@ -48,6 +49,15 @@ Route::get('/categories/{category:slug}', function (Category $category) {
         'title' => $category->name,
         'posts' => $category->posts,
         'category' => $category->name
+
+    ]);
+});
+
+Route::get('/authors/{author:username}', function(User $author) {
+    return view('posts', [
+        'title' => 'User Post',
+        'posts' => $author->posts,
+        
 
     ]);
 });
