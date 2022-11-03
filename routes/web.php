d5648b638ac71a25aea7c4c0d63cb5890957b4d7
etacodes <75968126+etacodes@users.noreply.github.com>
2022-11-04 02:11:58 +0700

    View Login & Registration

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,11 @@
 <?php
 
-use App\Models\Post;
+use App\Models\Category;
+
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
-
-use App\Models\Category;
-use App\Models\User;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
 
 /*
 |--------------------------------------------------------------------------
@@ -47,6 +47,10 @@ Route::get('/categories', function() {
 
     ]);
 });
+
+Route::get('/login', [LoginController::class, 'index']);
+
+Route::get('/register', [RegisterController::class, 'index']);
 
 
 
