94ed4791f485a51052093d2b8df7b7a90d09bace
etacodes <75968126+etacodes@users.noreply.github.com>
2022-11-05 19:00:17 +0700

    Dashboard UI

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -44,8 +44,13 @@ class Post extends Model
     }
 
 public function author()
-{
+    {
     return $this->belongsTo(User::class, 'user_id');
-}
+    }
+
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
 
 }
